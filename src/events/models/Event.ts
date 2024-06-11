2d14a4c01a4c96c30bb3c0261dd5b4e44d5edcc2
Shagdar Hamza <78723566+hamJaki@users.noreply.github.com>
2024-06-11 21:59:48 +0500

    first commit

--- src/events/models/Event.ts
+++ src/events/models/Event.ts
@@ -6,14 +6,16 @@ export interface IEvent extends Document {
     date: Date;
     location: string;
     duration: string;
+    city: string;
 }
 
 const EventSchema: Schema = new Schema({
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true},
     description: { type: String, required: true },
     date: { type: Date, default: Date.now },
     location: { type: String, required: true },
-    duration: { type: String, required: true }
+    duration: { type: String, required: true },
+    city: {type: String, required: true}
 });
 
 const Event = mongoose.model<IEvent>('Event', EventSchema);
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import globalRouter from './global-router';
 import { logger } from './logger';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 connectDB();
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/api/v1/',globalRouter);
 
 
-app.get('/helloworld',(request,response) =>{
+app.get('/',(request,response) =>{
   response.send("Hello World!");
 })
 
